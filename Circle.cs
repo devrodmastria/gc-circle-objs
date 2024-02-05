55e06a10d1038632db6908d01c50f623996cc228
Rod Mastria <rodmastria@gmail.com>
2024-02-04 22:46:52 -0500

    first commit

---
+++ Circle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleCalculator
+{
+    internal class Circle
+    {
+
+        // provided by user
+        private double radius;
+
+        public double pi = Math.PI;
+        public Circle(double newRadius)
+        {
+            radius = newRadius;
+        }
+
+        public double GetCircumference()
+        {
+            return 2 * pi * radius;
+        }
+
+        public double GetDiameter()
+        {
+            return 2 * radius;
+        }
+
+        public double GetArea() { 
+            return pi * radius * radius ;
+        }
+
+        public double GetRadius() { return radius; }
+
+        public void Grow()
+        {
+            radius *= 2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿// author: Rod M
+// task: Unit 4 - Lab 1
+
+using CircleCalculator;
+
+Console.WriteLine("------ Welcome to Circle Calculator! ------");
+Console.WriteLine("Enter your circle radius (integer or double)");
+
+Circle newCircle;
+
+while(true)
+{
+    if(double.TryParse(Console.ReadLine(), out double newRadius))
+    {
+
+        if (newRadius <= 0) {
+            Console.WriteLine("Invalid Radius. Please try a positive value");
+        }
+        else
+        {
+            newCircle = new Circle(newRadius);
+
+            Console.WriteLine("Diameter: {0}", newCircle.GetDiameter());
+            Console.WriteLine("Circumference: {0}", newCircle.GetCircumference());
+            Console.WriteLine("Area: {0}", newCircle.GetArea());
+
+
+            break;
+        }
+
+
+    }
+    else
+    {
+        Console.WriteLine("Invalid Radius. Please try a new value");
+    }
+}
+
+while(true)
+{
+    Console.WriteLine();
+    Console.WriteLine("Should the circle grow? y/n");
+
+    string choice = Console.ReadLine();
+    choice = choice.ToLower().Trim();
+    if (choice.Contains("y"))
+    {
+        newCircle.Grow();
+        Console.WriteLine("Diameter: {0}", newCircle.GetDiameter());
+        Console.WriteLine("Circumference: {0}", newCircle.GetCircumference());
+        Console.WriteLine("Area: {0}", newCircle.GetArea());
+        break;
+    }
+    else if (choice.Contains("n"))
+    {
+        Console.WriteLine();
+        Console.WriteLine("The circle's final radius is {0}", newCircle.GetRadius());
+        break;
+    }
+
+}
